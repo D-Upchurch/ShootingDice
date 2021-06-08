afe0310c36cb6f5b11df6c998ce0c341d4de9eff
Dakota <dakota.c.upchurch@gmail.com>
2021-06-08 14:57:51 -0500

    Sore Loser Player added and working

--- SoreLoserPlayer.cs
+++ SoreLoserPlayer.cs
@@ -1,11 +1,57 @@
+using System;
+
+
 namespace ShootingDice
 {
     // TODO: Complete this class
 
     // A Player that throws an exception when they lose to the other player
     // Where might you catch this exception????
-    public class SoreLoserPlayer
+    public class SoreLoserPlayer : Player
     {
-        
+        public override void Play(Player other)
+        {
+            // Call roll for "this" object and for the "other" object
+            int myRoll = Roll();
+            int otherRoll = other.Roll();
+
+            Console.WriteLine($"{Name} rolls a {myRoll}");
+            Console.WriteLine($"{other.Name} rolls a {otherRoll}");
+            try
+            {
+
+                if (myRoll > otherRoll)
+                {
+                    Console.WriteLine($"{Name} Wins!");
+                }
+                else if (myRoll < otherRoll)
+                {
+                    throw new LoseException("Sorry, I simply cannot lose.");
+                }
+                else
+                {
+                    // if the rolls are equal it's a tie
+                    Console.WriteLine("It's a tie");
+                }
+            }
+            catch (LoseException ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("I quit.");
+            }
+        }
+
     }
+
+    public class LoseException : Exception
+    {
+        public LoseException(string ex) : base(ex)
+        {
+
+        }
+    }
+
 }
